c3960f277ac8f1c9d1615682976f26219759eb5e
ryota0624 <tennis.suta6@gmail.com>
2022-06-20 10:12:39 +0900

    userからcarへのedgeを定義

--- start/start.go
+++ start/start.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/ryota0624/go-dev-with-ent/ent"
+	"github.com/ryota0624/go-dev-with-ent/ent/car"
 	"github.com/ryota0624/go-dev-with-ent/ent/user"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -22,20 +24,32 @@ func main() {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
-	if _, err := CreateUser(context.Background(), client); err != nil {
+	if _, err := CreateUser(context.Background(), client, "john"); err != nil {
 		log.Fatalf("failed creating user: %v", err)
 	}
 
-	if _, err := QueryUser(context.Background(), client); err != nil {
+	if _, err := QueryUser(context.Background(), client, "john"); err != nil {
 		log.Fatalf("failed query user: %v", err)
+	}
+
+	var a8m *ent.User
+
+	if u, err := CreateCars(context.Background(), client); err != nil {
+		log.Fatalf("failed creating cars: %v", err)
+	} else {
+		a8m = u
+	}
+
+	if err := QueryCars(context.Background(), a8m); err != nil {
+		log.Fatalf("failed query cars: %v", err)
 	}
 }
 
-func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+func CreateUser(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
 	u, err := client.User.
 		Create().
 		SetAge(30).
-		SetName("a8m").
+		SetName(name).
 		Save(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed creating user: %w", err)
@@ -45,10 +59,10 @@ func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 	return u, nil
 }
 
-func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+func QueryUser(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
 	u, err := client.User.
 		Query().
-		Where(user.Name("a8m")).
+		Where(user.Name(name)).
 		// `Only` fails if no user found,
 		// or more than 1 user returned.
 		Only(ctx)
@@ -57,4 +71,59 @@ func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 	}
 	log.Println("user returned: ", u)
 	return u, nil
+}
+
+func CreateCars(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	// Create a new car with model "Tesla".
+	tesla, err := client.Car.
+		Create().
+		SetModel("Tesla").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", tesla)
+
+	// Create a new car with model "Ford".
+	ford, err := client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", ford)
+
+	// Create a new user, and add it the 2 cars.
+	a8m, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		AddCars(tesla, ford).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", a8m)
+	return a8m, nil
+}
+
+func QueryCars(ctx context.Context, a8m *ent.User) error {
+	cars, err := a8m.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+	log.Println("returned cars:", cars)
+
+	// What about filtering specific cars.
+	ford, err := a8m.QueryCars().
+		Where(car.Model("Ford")).
+		Only(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+	log.Println(ford)
+	return nil
 }
