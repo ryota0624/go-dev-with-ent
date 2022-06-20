5f3039acf512e0fce32599de64ad05a6b5bc7d7b
ryota0624 <tennis.suta6@gmail.com>
2022-06-20 10:16:28 +0900

    user from carのedgeを定義

--- start/start.go
+++ start/start.go
@@ -43,6 +43,10 @@ func main() {
 	if err := QueryCars(context.Background(), a8m); err != nil {
 		log.Fatalf("failed query cars: %v", err)
 	}
+
+	if err := QueryCarUsers(context.Background(), a8m); err != nil {
+		log.Fatalf("failed query car users: %v", err)
+	}
 }
 
 func CreateUser(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
@@ -125,5 +129,21 @@ func QueryCars(ctx context.Context, a8m *ent.User) error {
 		return fmt.Errorf("failed querying user cars: %w", err)
 	}
 	log.Println(ford)
+	return nil
+}
+
+func QueryCarUsers(ctx context.Context, a8m *ent.User) error {
+	cars, err := a8m.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+	// Query the inverse edge.
+	for _, c := range cars {
+		owner, err := c.QueryOwner().Only(ctx)
+		if err != nil {
+			return fmt.Errorf("failed querying car %q owner: %w", c.Model, err)
+		}
+		log.Printf("car %q owner: %q\n", c.Model, owner.Name)
+	}
 	return nil
 }
