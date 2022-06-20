dcbfd0c1b9d75e570bdbfdf2420421c420a613fe
ryota0624 <tennis.suta6@gmail.com>
2022-06-20 17:49:27 +0900

    REST CRUD APIの自動生成拡張を追加

--- ent/entc.go
+++ ent/entc.go
@@ -5,13 +5,26 @@ package main
 import (
 	"log"
 
+	"ariga.io/ogent"
+	"entgo.io/contrib/entoas"
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 	"github.com/hedwigz/entviz"
+	"github.com/ogen-go/ogen"
 )
 
 func main() {
-	err := entc.Generate("./schema", &gen.Config{}, entc.Extensions(entviz.Extension{}))
+	spec := new(ogen.Spec)
+	oas, err := entoas.NewExtension(entoas.Spec(spec))
+	if err != nil {
+		log.Fatalf("creating entoas extension: %v", err)
+	}
+	ogent, err := ogent.NewExtension(spec)
+	if err != nil {
+		log.Fatalf("creating ogent extension: %v", err)
+	}
+
+	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(entviz.Extension{}, oas, ogent))
 	if err != nil {
 		log.Fatalf("running ent codegen: %v", err)
 	}
